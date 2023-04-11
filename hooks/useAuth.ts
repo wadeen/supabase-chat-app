fb658119ae9f974a0197a1ab3b73426452e3dfa8
wadeen <meister.vvdo@gmail.com>
2023-04-12 00:23:59 +0900

    Modify: sessionの型定義

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,8 +1,9 @@
 import supabase from "@/lib/supabase";
+import { Session } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 
 const useAuth = () => {
-  const [session, setSession] = useState<any>(); // ログイン状態を管理
+  const [session, setSession] = useState<Session | null>(null); // ログイン状態を管理
   const [error, setError] = useState(""); // エラー状況を管理
 
   useEffect(() => {
